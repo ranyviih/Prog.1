e5c220d2fca1fb35ad39f1e67a2c06c942aabcaf
ranyviih <124800291+ranyviih@users.noreply.github.com>
2023-08-10 09:10:26 -0300

    Add files via upload

---
+++ Repetição/Tv.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Tv {
+    final static Scanner LER = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n = 0; //dias
+        int s = 0; //saldo da conta no inicio
+        int saldo;
+        int mov = 0;
+
+        n = lerN(n);
+        s = lerS(s);
+
+        saldo = s;
+
+        for (int i = 0; i < n; i++) {
+            mov = lerMov(mov);
+            s += mov;
+
+            if (s < saldo) {
+                saldo = s;
+            }
+        }
+        imprimirSaldo(saldo);
+    }
+    public static int lerN(int n) {
+        do {
+            n = LER.nextInt();
+        } while (n <= 1 && n >= 30);
+        return n;
+    }
+    public static int lerS(int s) {
+        do {
+            s = LER.nextInt();
+        } while (s <= Math.pow(-10, 3) && s >= Math.pow(10, 3));
+        return s;
+    }
+    public static int lerMov(int mov) {
+        do {
+            mov = LER.nextInt();
+        } while (mov <= Math.pow(-10, 3) && mov >= Math.pow(10, 3));
+        return mov;
+    }
+    public static void imprimirSaldo(int saldo) {
+        System.out.println(saldo);
+    }
+}
